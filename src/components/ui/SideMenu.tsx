46069267938856f0bad3603eca98c370bebb4e46
Robert Castaneda <therobear@gmail.com>
2024-07-28 23:30:39 -0600

    Added links to side bar

--- src/components/ui/SideMenu.tsx
+++ src/components/ui/SideMenu.tsx
@@ -1,7 +1,58 @@
-import React from 'react';
+import React, { Fragment, useState } from 'react';
+import { Link } from 'react-router-dom';
 
-const SideMenu: React.FC = () => {
-    return <div>Side Menu</div>;
+const SideMenu = () => {
+    const [activeLink, setActiveLink] = useState('');
+
+    const setLIClass = (listEl: string) => {
+        if (activeLink === listEl) {
+            return 'sidbar-list-active';
+        } else {
+            return '';
+        }
+    };
+
+    return (
+        <Fragment>
+            <div className="sidebar-title">
+                <Link to="/" onClick={() => setActiveLink('')}>
+                    mcdb
+                </Link>
+            </div>
+            <Fragment>
+                <ul className="sidebar-nav">
+                    <li className={`${setLIClass('games')}`}>
+                        <Link to="games" onClick={() => setActiveLink('games')}>
+                            Games
+                        </Link>
+                    </li>
+                    <li className={`${setLIClass('movies')}`}>
+                        <Link
+                            to="movies"
+                            onClick={() => setActiveLink('movies')}
+                        >
+                            Movies
+                        </Link>
+                    </li>
+                    <li className={`${setLIClass('music')}`}>
+                        <Link to="music" onClick={() => setActiveLink('music')}>
+                            Music
+                        </Link>
+                    </li>
+                    <li className={`${setLIClass('books')}`}>
+                        <Link to="books" onClick={() => setActiveLink('books')}>
+                            Books
+                        </Link>
+                    </li>
+                    <li className={`${setLIClass('about')}`}>
+                        <Link to="about" onClick={() => setActiveLink('about')}>
+                            About
+                        </Link>
+                    </li>
+                </ul>
+            </Fragment>
+        </Fragment>
+    );
 };
 
 export default SideMenu;
